794d9f70ba83587846d4e9f1eb70065b8ac2a9b1
Lourdes Billone <lou.billone@gmail.com>
2021-11-28 21:42:44 -0300

    PRIMERA ENTREGA JS

---
+++ main.js
@@ -0,0 +1,10 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+
+console.log('Su nombre completo es ' +nombre + " " + apellido);
+
+let añoNac = parseInt(prompt("Ingrese su año de nacimiento"));
+let añoActual = 2021;
+let resultado = añoActual - añoNac;
+
+console.log(`Usted tiene ` +resultado + ` años`);
